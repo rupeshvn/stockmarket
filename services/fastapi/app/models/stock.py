7f3e09edb596f5d4fac19c88b711aa1e7b9e2b91
rupeshvn <codewithrupeshv@gmail.com>
2026-04-01 21:29:47 +0530

    Added the alembic models

--- services/fastapi/app/models/stock.py
+++ services/fastapi/app/models/stock.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Column, Float, DateTime
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from app.db.base import Base
 
 class StockData(Base):
     __tablename__ = "stock_data"
